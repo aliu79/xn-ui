ce4893068a9818c70ac4806c60721babc998a553
liuzengwei <604806892@qq.com>
2022-05-31 18:54:34 +0800

    update

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -66,7 +66,7 @@ const download = (params = { name: '', url: '' }) => {
   var x = new XMLHttpRequest()
   x.open('GET', url, true)
   x.responseType = 'blob'
-  x.onload = function (e) {
+  x.onload = function () {
     // 会创建一个 DOMString，其中包含一个表示参数中给出的对象的URL。这个 URL 的生命周期和创建它的窗口中的 document 绑定。这个新的URL 对象表示指定的 File 对象或 Blob 对象。
     var url = window.URL.createObjectURL(x.response)
     var a = document.createElement('a')
