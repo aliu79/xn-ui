c5223fbc705b474525625e8e5aed53775020d6ad
刘增伟 <liuzengwei@xianniu.cn>
2022-10-19 15:38:03 +0800

    update

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,37 +1,115 @@
 <template>
-  <xn-upload
-    :fileList.sync="fileList"
-    :limit="limit"
-    @on-success="onSuccess"
-  ></xn-upload>
+  <xn-table
+    :columns="listHeader"
+    :data="list"
+    :page="pageConfig"
+    @handle-buttons="handleButtons"
+    @on-page="fnGetList"
+  >
+    <!-- <template #tools>
+      <div>
+        123
+      </div>
+    </template> -->
+  </xn-table>
+
 </template>
 
 <script>
 export default {
   data() {
     return {
-      limit: 2,
-      config: {
-        accept: "image", //接受上传的文件类型：zip、pdf、excel、image，也可以是文件类型所组成的数组类型如：['image', 'pdf']，则只可以上传图片或pdf类型的文件，也可以为空，则任何类型的文件都可以上传
-        max: 100, //文件大小
-      },
-      fileList: [
+      listHeader: [
+        { prop: "id", label: "ID" },
+        { prop: "name", label: "姓名" },
+        { prop: "age", label: "年龄", labelMsg: "表头字段说明" },
+        { prop: "date", label: "日期", sortable: true },
+        { prop: "age", label: "年龄", labelMsg: "表头字段说明" },
+        { prop: "date", label: "日期", sortable: true },
+        { prop: "age", label: "年龄", labelMsg: "表头字段说明" },
+        { prop: "date", label: "日期", sortable: true },
+        { prop: "age", label: "年龄", labelMsg: "表头字段说明" },
+        { prop: "date", label: "日期", sortable: true },
+        { prop: "age", label: "年龄", labelMsg: "表头字段说明" },
+        { prop: "date", label: "日期", sortable: true },
+        { prop: "age", label: "年龄", labelMsg: "表头字段说明" },
+        { prop: "date", label: "日期", sortable: true },
+        { prop: "age", label: "年龄", labelMsg: "表头字段说明" },
+        { prop: "date", label: "日期", sortable: true },
         {
-          url: "https://i.xianniu.cn/accessory/2022/08/04/b194177e9642454480726feb2ce842fa.jpg",
+          label: "图片",
+          render: (h) => {
+            return h("div", {}, [
+              h("img", {
+                attrs: {
+                  src: "https://i.xianniu.cn/accessory/2022/08/04/b194177e9642454480726feb2ce842fa.jpg",
+                },
+                style: {
+                  width: "100px",
+                },
+              }),
+              h("span", "文字文字文字文字"),
+            ]);
+          },
+        },
+        {
+          label: "操作",
+          fixed: "right",
+          more: {
+            options: [
+              {
+                label: "按钮",
+                icon: "el-icon-info",
+                method: "info",
+              },
+              {
+                label: "删除",
+                method: "remove",
+                isPopConfirm:true,
+                options: {
+                },
+              },
+
+            ],
+          },
         },
       ],
+      list: [
+
+        { id: 122, date: "2011-01-01", name: "lzw", age: 18 },
+        { id: 2, date: "2011-01-03", name: "lzw", age: 22 },
+      ],
+      pageConfig: {
+        total: 100,
+        pageSize: 15,
+        pageNum: 1,
+      },
+      tools:[
+        {label:'导出',prop:'export',icon:'el-icon-files'},
+      ],
+      // pageConfig: {},
     };
   },
+  created() {},
   methods: {
-    onSuccess(val) {
+    handleSort(e) {
+      console.log(e);
+    },
+    handleRefresh() {
+      console.log("re");
+    },
+    handleButtons(args) {
+      console.log(args);
+    },
+    fnGetList(v) {
+      console.log(v);
+    },
+    onradio(val) {
+      console.log("val: ", val);
+    },
+    onSelection(val) {
       console.log(val);
-    },
-    beforeUpload(val) {
-      console.log("val", val);
-    },
-    onProgress() {},
-    onPreview() {},
-    onRemove() {},
+    }
   },
 };
 </script>
