1185118189e296078a84194f61f0a3c4ea909d6c
刘增伟 <liuzengwei@xianniu.cn>
2023-01-02 18:27:10 +0800

    update

--- src/utils/index.js
+++ src/utils/index.js
@@ -10,13 +10,21 @@ const version = () => {
     return `xianniu-tools@${require('./package.json').version}`
 }
 
+export const $reg = reg
+export const $format = format
+export const $dayjs = dayjs
+export const $storage = storage
+export const $utils = utils
+export const $lodash = lodash
+export const $math = math
+
 export default {
     version,
-    $reg: reg,
-    $format: format,
-    $dayjs: dayjs,
-    $lodash: lodash,
-    $storage: storage,
-    $utils: utils,
-    $math: math
+    $reg,
+    $format,
+    $dayjs,
+    $lodash,
+    $storage,
+    $utils,
+    $math
 }
