3d14f2eca751ee036416001854259272b7096b1c
刘增伟 <liuzengwei@xianniu.cn>
2023-04-12 15:25:51 +0800

    update

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,13 @@ const plugins = [];
 if (process.env.NODE_ENV === 'production') {
   plugins.push("transform-remove-console")
 }
+plugins.push([
+  "component",
+  {
+    "libraryName": "element-ui",
+    "styleLibraryName": "theme-chalk"
+  }
+])
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset',
